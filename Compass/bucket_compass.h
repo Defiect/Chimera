5efb42d9be7ad22e6ad6c2a7423de532656444c2
Defiect <39240362+Defiect@users.noreply.github.com>
2020-01-16 18:33:53 -0600

    Added inital angle calibration code

--- Compass/bucket_compass.h
+++ Compass/bucket_compass.h
@@ -10,8 +10,9 @@ SoftwareSerial CMPS12 = SoftwareSerial(2, 3);
 unsigned char high_byte, low_byte, angle8;
 char pitch, roll;
 unsigned int angle16;
+float startingHeading;
 
-float getHeading() {
+float getRawHeading() {//get raw compass reading from cmps12 sensor
   CMPS12.write(CMPS_GET_ANGLE16);  // Request and read 16 bit angle
   while (CMPS12.available() < 2);
   high_byte = CMPS12.read();
@@ -24,4 +25,24 @@ float getHeading() {
   angleDecimal /= 10;
 
   return angleDecimal;
+}
+
+float transferHeading(float heading) { //translate compass reading into coordinateMotion.h facingAngle format
+  float newHeading = 360 - heading; //convert from clockwise to counter-clockwise format
+  return newHeading;
+}
+
+float getHeading() { //get angle from cmps12 sensor in facingAngle format
+  float heading = transferHeading(getRawHeading());
+  return heading;
+}
+
+float getCalibratedHeading() {
+
+  float calibratedHeading = getHeading() - startingHeading;
+
+  if (calibratedHeading < 0) {
+    calibratedHeading = 360 + calibratedHeading;
+  }
+  return calibratedHeading;
 }
--- Compass/working_compass_library.ino
+++ Compass/working_compass_library.ino
@@ -1,17 +1,22 @@
 #include "bucket_compass.h"
 
+
 void setup()
 {
   Serial.begin(9600);              // Start serial ports
   CMPS12.begin(9600);
+
+  startingHeading = getHeading();
 }
 
 void loop()
 {
-  float heading = getHeading();
+  float heading = getCalibratedHeading();
 
   Serial.print("Heading: ");       // Display 16 bit angle with decimal place
   Serial.println(heading);
 
+  Serial.print("Starting Heading: ");       // Display 16 bit angle with decimal place
+  Serial.println(startingHeading);
   delay(1000);                           // Short delay before next loop
 }
